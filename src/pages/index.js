a5c2ef3a52ea1af949bdaff6929fda810772a009
cjaeger <chris_jaeger@nylim.com>
2018-04-13 09:53:43 -0400

    basic home html

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="business-card">
+    <h1><span className="first">Christopher</span> <span className="last">Jaeger</span></h1>
+    <p className="tagline">Front End Craftsman</p>
+    {/*<h2>Portfolio: The web is my workshop</h2>*/}
+    <div class="contact-me">
+      <a href="https://www.linkedin.com/in/christopher-jaeger-5b9b4226" className="linkedin">linkedin</a>
+      <a href="https://twitter.com/seeJaeger" className="twitter">twitter</a>
+      <a href="https://github.com/jaegz" className="github">github</a>
+      <a href="https://codepen.io/jaegs" className="codepen">codepen</a>
+      <a href="https://stackoverflow.com/users/2561435/jaegs" className="stackoverflow">stackoverflow</a>
+      <a href="mailto:chris@seejaeger.com" className="email">email</a>
+    </div>
+    {/*<Link to="/page-2/">Go to page 2</Link>*/}
   </div>
 )
 
